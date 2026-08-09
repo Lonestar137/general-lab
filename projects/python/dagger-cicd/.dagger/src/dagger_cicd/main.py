43b309e8235846b38370fdee930c90144ccc2eda
Garrett Jones <garretcj2010@hotmail.com>
2026-08-09 01:18:17 -0500

    Add wheelhouse build function

--- projects/python/dagger-cicd/.dagger/src/dagger_cicd/main.py
+++ projects/python/dagger-cicd/.dagger/src/dagger_cicd/main.py
@@ -10,6 +10,16 @@ class DaggerCicd:
         return dag.container().from_("alpine:latest").with_exec(["echo", string_arg])
 
     @function
+    async def container_echo_test(self, string_arg: str) -> str:
+        """Returns a container that echoes whatever string argument is provided"""
+        return await (
+            dag.container()
+            .from_("alpine:latest")
+            .with_exec(["echo", string_arg])
+            .stdout()
+        )
+
+    @function
     async def grep_dir(self, directory_arg: dagger.Directory, pattern: str) -> str:
         """Returns lines that match a pattern in the files of the provided Directory"""
         return await (
@@ -22,16 +32,47 @@ class DaggerCicd:
         )
 
     @function
-    async def build_wheelhouse(
-        self, directory_arg: dagger.Directory
-    ) -> dagger.Container:
+    async def build_wheelhouse_dev(self, directory_arg: dagger.Directory) -> str:
         """Build the wheelhouse"""
         return await (
             dag.container()
             .from_("python:3.14-slim")
             .with_mounted_directory("/app", directory_arg)
             .with_workdir("/app")
+            .with_exec(["pip", "install", "-e", "."])
             .with_exec(["pip", "wheel", "-w", "wheelhouse", "."])
-            .with_workdir("./wheelhouse")
-            .with_exec(["pip", "download", "-d", ".", "../"])
+            .with_exec(
+                [
+                    "pip",
+                    "freeze",
+                    "--exclude-editable",
+                ]
+            )
+            .stdout()
+        )
+
+    @function
+    async def build_wheelhouse(
+        self, directory_arg: dagger.Directory
+    ) -> dagger.Directory:
+        """Build the wheelhouse"""
+        return await (
+            dag.container()
+            .from_("python:3.14-slim")
+            .with_mounted_directory("/app", directory_arg)
+            .with_workdir("/app")
+            .with_exec(["pip", "install", "-e", "."])
+            .with_exec(["pip", "wheel", "-w", "wheelhouse", "."])
+            .with_exec(
+                [
+                    "sh",
+                    "-c",
+                    "pip freeze --exclude-editable > requirements.lock",
+                ]
+            )
+            .with_exec(
+                ["pip", "download", "-d", "/app/wheelhouse", "-r", "requirements.lock"]
+            )
+            .with_exec(["mv", "requirements.lock", "/app/wheelhouse"])
+            .directory("/app/wheelhouse")
         )
---
+++ projects/python/dagger-cicd/README.md
@@ -0,0 +1,11 @@
+
+
+# Dagger
+
+```bash
+
+dagger functions
+
+# Build and export wheelhouse as an artifact.
+dagger call build-wheelhouse --directory-arg=. export --path ./wheelhouse-out
+```
